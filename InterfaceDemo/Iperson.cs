dce6d56f75d998655a43090bd47bb7ce5ea9f865
parlaTharuni <tharuniparla@gmail.com>
2023-10-27 16:17:05 +0530

    Add project files.

---
+++ InterfaceDemo/Iperson.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDemo
+{
+    internal interface Iperson
+    {
+        string getInfo();
+        string getTypeOfPerson();
+
+    }
+}
